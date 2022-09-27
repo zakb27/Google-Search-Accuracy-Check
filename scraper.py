d565b99a13bfea1339a7d1cbe8d647d7f439eb82
zakb27 <zakbrook02@gmail.com>
2022-09-27 15:17:09 +0100

    Prettified words

--- scraper.py
+++ scraper.py
@@ -1,8 +1,14 @@
 import requests
+import re
 from bs4 import BeautifulSoup
 
-URL = "http://www.values.com/inspirational-quotes"
+URL = "https://finmead.weebly.com/"
 r = requests.get(URL)
 
 soup = BeautifulSoup(r.content,'html.parser')  # If this line causes an error, run 'pip install html5lib' or install html5lib
-print(soup.prettify())
+all_text = soup.get_text(strip=True, separator=u' ')
+all_text = re.sub('\\s', ' ', all_text)
+all_text = re.sub('[0-9]', ' ', all_text)
+all_text = re.sub('[ ]{2,}', ' ', all_text)
+
+print(all_text)
